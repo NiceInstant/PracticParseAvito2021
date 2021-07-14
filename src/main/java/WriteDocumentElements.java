28c2d61f96eaaad6826f0b6fe393880454b50dbd
NiceInstant <53512781+NiceInstant@users.noreply.github.com>
2021-07-14 16:13:28 +0300

    Add files via upload

--- src/main/java/WriteDocumentElements.java
+++ src/main/java/WriteDocumentElements.java
@@ -33,53 +33,71 @@ public class WriteDocumentElements {
                 /*Elements dives = document.select("a[data-marker=item-title]");
                 Elements h3Titles = dives.select("h3");*/
 
-        Elements divTitles= document.select("div[class = ListingItem-module__main]");
+        Elements divTitles = document.select("div[class = ListingItem-module__main]");
 
         List<String> first_divTitleList = new ArrayList<>();
         List<String> second_divTitleList = new ArrayList<>();
 
 
-        for(Element title:divTitles)
-        {
+        for (Element title : divTitles) {
 
-            String stringTitle = "Марка авто:"+title.select("a[class = Link ListingItemTitle__link]").text()
-                            +"\t Цена:"+title.select("div[class = ListingItemPrice-module__content]").text();
+            String stringTitle = "Марка авто:" + title.select("a[class = Link ListingItemTitle__link]").text()
+                    + "\t Цена:" + title.select("div[class = ListingItemPrice-module__content]").text();
             first_divTitleList.add(stringTitle);
             second_divTitleList.add(stringTitle);
 
         }
         Collections.sort(first_divTitleList);
         Collections.sort(second_divTitleList);
+
         System.out.println(first_divTitleList);
         System.out.println("===================================");
-        System.out.println("Сравнение первого и второго списка"+first_divTitleList.equals(second_divTitleList));
-
-        Path fileTitleList = Paths.get("fileTitleList.txt");
-        Files.write(fileTitleList,first_divTitleList, StandardCharsets.UTF_8);
+        System.out.println("Сравнение первого и второго списка" + first_divTitleList.equals(second_divTitleList));
 
         File file = new File("fileTitleList.txt");
+        Reader reader = new FileReader(file);
+        BufferedReader brReader = new BufferedReader(reader);
+
+        String line;
+        List<String> lineList = new ArrayList<>();
+        while ((line = brReader.readLine()) != null) {
+            /*System.out.println(line);*/
+            lineList.add(line);
+        }
+
+        boolean equals = second_divTitleList.equals(lineList);
+        if (equals == true)
+            System.out.println("Объявлений новых нет");
+            else
+            {
+                System.out.println("Есть новые объявления");
+                Path fileTitleList = Paths.get("fileTitleList.txt");
+                Files.write(fileTitleList, first_divTitleList, StandardCharsets.UTF_8);
+            }
+
+        /*File file = new File("fileTitleList.txt");
 
         Reader reader = new FileReader(file);
         BufferedReader brReader = new BufferedReader(reader);
 
         String line;
         List<String> lineList = new ArrayList<>();
-        while((line = brReader.readLine()) != null) {
+        while ((line = brReader.readLine()) != null) {
             System.out.println(line);
             lineList.add(line);
-        }
+        }*/
 
         brReader.close();
-        System.out.println(lineList.size());
-        System.out.println("Список из документа"+lineList);
-        System.out.println("=======================");
-        System.out.println(second_divTitleList.size());
-        System.out.println("Второй список"+second_divTitleList);
+        /*System.out.println(lineList.size());*/
+        System.out.println("Список из документа" + lineList);
+        /*System.out.println("=======================");
+        System.out.println(second_divTitleList.size());*/
+        System.out.println("Второй список" + second_divTitleList);
 
         /*System.out.println("Сравнение второго списка со списком из документа :"+second_divTitleList.equals(lineList));*/
 
-        boolean equals = second_divTitleList.equals(lineList);
-        if (equals == true) System.out.println("Объявлений новых нет");
+        /*boolean equals = second_divTitleList.equals(lineList);
+        if (equals == true) System.out.println("Объявлений новых нет");*/
 
     }
 }
