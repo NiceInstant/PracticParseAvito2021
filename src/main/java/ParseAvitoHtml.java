0ba8f7e6746ab20169216c461ad835e01fbc2b1b
NiceInstant <53512781+NiceInstant@users.noreply.github.com>
2021-07-14 11:45:47 +0300

    Add files via upload

--- src/main/java/ParseAvitoHtml.java
+++ src/main/java/ParseAvitoHtml.java
@@ -1,29 +1,20 @@
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
-import org.jsoup.select.Elements;
 
-
-import java.io.File;
-import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 
 
 public class ParseAvitoHtml {
     private static Document getDocument() throws IOException {
-        String url = "https://www.avito.ru/naberezhnye_chelny/avtomobili?cd=1&radius=200";
+        /*String url = "https://www.avito.ru/naberezhnye_chelny/avtomobili?cd=1&radius=200";*/
+
+        String url = "https://auto.ru/naberezhnye_chelny/cars/all/";
+
         Document document = Jsoup.connect(url).get();
 
 
@@ -34,28 +25,35 @@ public class ParseAvitoHtml {
 
             public static void main(String[] args) throws IOException {
                 Document document = getDocument();
-                Elements dives = document.select("a[data-marker=item-title]");
+                /*Elements dives = document.select("a[data-marker=item-title]");
+                Elements h3Titles = dives.select("h3");*/
 
-                Elements h3Titles = dives.select("h3");
-
-
-                List<String> first_h3TitleList = new ArrayList<>();
-                List<String> second_h3TitleList = new ArrayList<>();
+                Elements divTitles= document.select("div[class = ListingItem-module__main]");
 
 
-                for(Element title:h3Titles)
+
+
+                List<String> first_divTitleList = new ArrayList<>();
+                List<String> second_divTitleList = new ArrayList<>();
+
+
+                for(Element title:divTitles)
                 {
-                    String stringTitle = title.text()+"\n";
-                    first_h3TitleList.add(stringTitle);
+                    /*String stringTitle = title.text()+"\n";*/
+                    String stringTitle = "Марка авто:"+title.select("a[class = Link ListingItemTitle__link]").text()+"\t Цена:"
+                            +title.select("div[class = ListingItemPrice-module__content]").text()+"\n";
+                    first_divTitleList.add(stringTitle);
 
                     /*System.out.println(stringTitle);*/
                 }
-                Collections.sort(first_h3TitleList);
-                System.out.println(first_h3TitleList);
+                Collections.sort(first_divTitleList);
+                System.out.println(first_divTitleList);
                 System.out.println("===================================");
 
-                Path fileTitleList = Paths.get("fileTitleList.txt");
-                Files.write(fileTitleList,first_h3TitleList, StandardCharsets.UTF_8);
+
+
+               /* Path fileTitleList = Paths.get("fileTitleList.txt");
+                Files.write(fileTitleList,first_divTitleList, StandardCharsets.UTF_8);*/
 
         }
     }
